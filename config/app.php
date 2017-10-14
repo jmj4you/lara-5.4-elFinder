438ef0fedf604b2ced53e828785eb8396b726796
Gogs <the94air@gmail.com>
2017-10-14 16:45:47 +0300

    Fixing errors

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        Barryvdh\Elfinder\ElfinderServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -176,7 +177,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Barryvdh\Elfinder\ElfinderServiceProvider::class
 
     ],
 
